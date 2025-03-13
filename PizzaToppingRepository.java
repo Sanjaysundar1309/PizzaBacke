62decd7a5f28a8372a9139f07b1821132bec3cc2
LeelavathiMB <leelavathimb1921@gmail.com>
2025-03-13 16:05:19 +0530

    Delete PizzaToppingRepository.java

--- PizzaToppingRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.Pizza;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PizzaToppingRepository extends JpaRepository<PizzaTopping, Long> {
-}
