bafa74f2fe37da8698dd67952779eaa275d4f3e0
priyakc23 <priyakc8971@gmail.com>
2025-03-13 16:05:18 +0530

    Delete PizzaNotFoundException.java

--- PizzaNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.Pizza;
-
-public class PizzaNotFoundException extends RuntimeException {
-    public PizzaNotFoundException(String message) {
-        super(message);
-    }
-}
