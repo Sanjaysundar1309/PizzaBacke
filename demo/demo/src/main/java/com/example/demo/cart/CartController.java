e55a8810308b92bcbb4a0b02343f5de3219c89f4
Sanjaysundar1309 <sanjaysundar1309@gmail.com>
2025-03-14 11:29:08 +0530

    comm

--- demo/demo/src/main/java/com/example/demo/cart/CartController.java
+++ demo/demo/src/main/java/com/example/demo/cart/CartController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import jakarta.validation.Valid;
 
 import java.util.List;
-
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/cart")
 public class CartController {
